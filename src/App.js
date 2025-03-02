b2d81cfef27e009963eab895df910198a6c8e61d
CHANUKA RANDITHA <155728444+Chanuka999@users.noreply.github.com>
2025-03-02 08:30:33 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import react, { useState, useEffect } from "react";
 import "./styles.css";
+import Navbar from "./Components/Navbar";
+//import { css } from "@emotion/react";
+import { PropagateLoader } from "react-spinners";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [loading, setLoding] = useState(false);
+  const override = CSS`
+  display:block;
+  border-color:red;
+  margin-top:20%;`;
+
+  useEffect(() => {
+    setLoding(true);
+    setTimeout(() => {
+      setLoding(false);
+    }, 5000);
+  }, []);
+  return (
+    <div className="App">
+      {loading ? (
+        <PropagateLoader
+          color={"#3d2514"}
+          loading={"loading"}
+          css={"override"}
+          size={40}
+        />
+      ) : (
+        <>
+          <Navbar />
+        </>
+      )}
+    </div>
+  );
 }
